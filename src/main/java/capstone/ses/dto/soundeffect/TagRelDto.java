6c3bfad5e19a5e7eaa00079a39d962b65a414418
hayeongKo <polarpheno@icloud.com>
2024-05-23 23:49:18 +0900

    [FEAT] tag rel 저장 로직 추가

---
+++ src/main/java/capstone/ses/dto/soundeffect/TagRelDto.java
@@ -0,0 +1,11 @@
+package capstone.ses.dto.soundeffect;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TagRelDto {
+    private Long id;
+    private String tags;
+}
