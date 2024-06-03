e74544d3aa0acf5be248b1bd3624fc12b016c807
hayeongKo <polarpheno@icloud.com>
2024-06-04 04:35:10 +0900

    [REFACTOR] member field 수정

--- src/main/java/capstone/ses/domain/member/Member.java
+++ src/main/java/capstone/ses/domain/member/Member.java
@@ -3,6 +3,7 @@ package capstone.ses.domain.member;
 import capstone.ses.domain.system.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,4 +17,12 @@ public class Member extends BaseEntity {
     private Long id;
     private String name;
     private String email;
+    private String picture;
+
+    @Builder
+    public Member(String name, String email, String picture) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
 }
