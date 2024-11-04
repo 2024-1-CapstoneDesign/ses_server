88fe961c24698284781ccf43569d9208cc1f926e
hayeongKo <polarpheno@icloud.com>
2024-11-04 20:15:51 +0900

    [FEAT] 구글 소셜 로그인 로직 추가

---
+++ src/main/java/capstone/ses/controller/AuthController.java
@@ -0,0 +1,28 @@
+package capstone.ses.controller;
+
+import capstone.ses.dto.jwt.AuthTokens;
+import capstone.ses.dto.system.Result;
+import capstone.ses.dto.system.ResultCode;
+import capstone.ses.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/oauth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @GetMapping("/login")
+    public Result login(@RequestParam String accessToken) {
+        try {
+            return new Result<>(ResultCode.SUCCESS, authService.loginByAccessToken(accessToken));
+        } catch (Exception e) {
+            return new Result<>(ResultCode.FAIL, e.getMessage(), "401");
+        }
+    }
+}
