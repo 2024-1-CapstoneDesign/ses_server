f945ac4cc6e814494a1166a03f0aad09776e5302
hayeongKo <polarpheno@icloud.com>
2024-11-04 18:20:43 +0900

    [REFACTOR] 불필요한 import 제거

--- src/main/java/capstone/ses/service/SoundEffectService.java
+++ src/main/java/capstone/ses/service/SoundEffectService.java
@@ -7,7 +7,6 @@ import capstone.ses.repository.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.json.JsonReadFeature;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
