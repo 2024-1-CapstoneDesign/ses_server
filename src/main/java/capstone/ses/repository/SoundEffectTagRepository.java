fa6bb950c32b9e515e5d59666caa191539e9ac3a
hayeongKo <polarpheno@icloud.com>
2024-11-27 14:54:28 +0900

    [REFACTOR] createBy 주석처리

--- src/main/java/capstone/ses/repository/SoundEffectTagRepository.java
+++ src/main/java/capstone/ses/repository/SoundEffectTagRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SoundEffectTagRepository extends JpaRepository<SoundEffectTag, Long> {
     SoundEffectTag findByName(String name);
+    boolean existsByName(String name);
 }
